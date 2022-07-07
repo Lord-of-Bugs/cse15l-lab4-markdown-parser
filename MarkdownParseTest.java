453c4e9ec94927aa15f27bf9acb57863c104941a
Muchan Li <limuchan28@gmail.com>
2022-07-07 09:30:48 -0700

    one test case for each of the 8 files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,4 +23,64 @@ public class MarkdownParseTest {
         assertTrue(expected.equals(actual));
     }
 
+    @Test
+    public void testGetLinks1() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file2.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("https://google.com", "some-thing.html");
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testGetLinks2() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file3.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("more text here");
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testGetLinks3() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file5.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("page.com");
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testGetLinks5() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file6.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("page.com");
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testGetLinks6() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file7.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void testGetLinks7() throws IOException {
+        Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file8.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("a link on the first line");
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        assertTrue(expected.equals(actual));
+    }
 }
