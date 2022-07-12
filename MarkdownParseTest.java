9274189a46094799c8cb826037642f6cd7bb38ac
Muchan Li <limuchan28@gmail.com>
2022-07-12 08:22:28 -0700

    failure-inducing test case

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,7 +18,7 @@ public class MarkdownParseTest {
     public void testGetLinks() throws IOException {
         Path fileName = Paths.get("/Users/Muchan/Desktop/UCSD-2021-2022/CSE15L/cse15l-lab-reports/cse15l-lab4-markdown-parser/test-file.md");
         String content = Files.readString(fileName);
-        List<String> expected = List.of("https://something.com", "some-thing.html");
+        List<String> expected = List.of("https://something.com", "Some-thing.html");
         ArrayList<String> actual = MarkdownParse.getLinks(content);
         assertTrue(expected.equals(actual));
     }
